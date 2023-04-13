76fa7538a1aa3ac14a8bf35b73a60a1ecd2c9564
Eduardo <eduardo.hbds@gmail.com>
2023-04-13 10:56:07 -0300

    Teste code refator

--- musicDownloaded - Copia/Teste-code.py
+++ musicDownloaded - Copia/Teste-code.py
@@ -4,14 +4,21 @@ import youtube_dl # client to many multimedia portals
 # downloads yt_url to the same directory from which the script runs
 def download_audio(yt_url):
     ydl_opts = {
+        'writethumbnail': True,
         'format': 'bestaudio/best',
+        'ignoreerrors':True,
+        'keepvideo':False,
+        'cachedir':False,
+        # name the file the ID of the video
+        'nooverwrites': True,
+        'add-metadata':True,    
+        'outtmpl': '%(artist)s-%(title)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
-            'cachedir':'false',
-            
-        }],
+        },
+            {'key': 'EmbedThumbnail', }, ],
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([yt_url])
